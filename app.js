fb5131dda2d3362e2fc63e57d8fe6ae3c9213f0d
Maylayloo <milosz.l-24@wp.pl>
2022-06-13 16:34:23 +0200

    Initial commit + board and player movement are done

---
+++ app.js
@@ -0,0 +1,13 @@
+const PLAYER1 = 'fa-circle-o';
+const PLAYER2 = 'fa-times';
+let round = 1;
+
+const boxes = [...document.querySelectorAll('.box')];
+boxes.forEach(box => box.addEventListener('click', pick));
+
+function pick(event) {
+    const turn = round%2 === 0 ? PLAYER2 : PLAYER1;
+    event.target.classList.add(turn)
+    round++;
+
+}
